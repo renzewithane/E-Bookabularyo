f12a33b87192448da16eb8416fc26e434f6fb620
tozen123 <lanzuela.christian@gmail.com>
2023-11-16 11:12:11 +0800

    v0.1a1

--- App/app/src/main/java/com/teamloopit/e_bookabularyo/MainActivity.java
+++ App/app/src/main/java/com/teamloopit/e_bookabularyo/MainActivity.java
@@ -2,15 +2,25 @@ package com.teamloopit.e_bookabularyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isNewUser;
+    private final boolean NewUserKey;
+    private SharedPreferences mPreferences;
+    public MainActivity(boolean NewUser) {
+        this.NewUserKey = NewUser;
+    }
+
+    private String sharedPrefFile = "com.teamloopit.e_bookabularyo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
 
 
     }
